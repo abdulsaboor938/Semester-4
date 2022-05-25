5293b253dfae75d8c602c5fdbd8809739e4a6bc6
Abdul Saboor <66201863+abdulsaboor938@users.noreply.github.com>
2022-05-25 22:49:58 +0500

    mac

---
+++ Big Data Analytics/Code/Lab/Lab 11/Task1.py
@@ -0,0 +1,14 @@
+# Abdul Saboor
+# 20L-1113
+# BDS-4A
+
+# Remove punctuation marks from all text and also convert text into lower case using map reducer
+# Expected sample output
+# ali works on sunday
+
+import sys  # importing library for reading from stdin
+for line in sys.stdin:  # looping for every line from stdin
+    for char in line:  # looping for every character in line
+        if (char not in ['.', '!', '-']):  # checking if not punctuation
+            sys.stdout.write(char)  # printing if not char
+        # no printing if punctuation
