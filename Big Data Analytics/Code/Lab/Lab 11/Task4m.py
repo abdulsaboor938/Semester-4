59f985b93f7e56e6bc8ed355d92c78533845dd52
Abdul Saboor <66201863+abdulsaboor938@users.noreply.github.com>
2022-06-13 13:32:27 +0500

    win

---
+++ Big Data Analytics/Code/Lab/Lab 11/Task4m.py
@@ -0,0 +1,13 @@
+# Abdul Saboor
+# 20L-1113
+# BDS-4A
+# Task 3 Mapper
+
+# Count how many total times has each person written
+# Expected sample output
+# Ali 3
+
+import sys  # importing library for reading from stdin
+for line in sys.stdin:  # looping for every line from stdin
+    sys.stdout.write(line)  # outputting to std out
+    # no printing if punctuation
